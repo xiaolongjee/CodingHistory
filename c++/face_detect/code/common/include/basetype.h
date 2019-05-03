7bd3c3dcb801be079f4c0f9ed71cbf2049af0b25
xiaolongjee <xiaolongjee@mail.nwpu.edu.cn>
2019-05-03 17:16:28 +0800

    delete C++ proc

--- c++/face_detect/code/common/include/basetype.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BASETYPE_H_201611131432
-#define __BASETYPE_H_201611131432
-
-typedef unsigned char UCHAR;
-typedef UCHAR	      BYTE;
-typedef short int     WORD16;
-typedef int           WORD32;
-
-#endif
